66b10a8430c7887eec6d10c74c8b94f8165fcb80
ebakernz <emma@psdigital.co.nz>
2026-03-12 13:23:30 +1300

    typo

--- src/SearchPageController.php
+++ src/SearchPageController.php
@@ -4,7 +4,7 @@ namespace PlasticStudio\Search;
 
 use PageController;
 use SilverStripe\Model\List\ArrayList;
-use SilverStripe\Model\List\PaginatedList
+use SilverStripe\Model\List\PaginatedList;
 use SilverStripe\ORM\DB;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\View\Requirements;
